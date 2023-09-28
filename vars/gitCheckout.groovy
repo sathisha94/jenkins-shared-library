ded965f677cfc7bf4cd0a43c41014d74567c2302
Harsha Jain <jaintpharsha@gmail.com>
2023-09-28 09:20:15 +0530

    Just Checkout 28-September-23 09:20AM

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,7 @@
 def call(String git_branch = 'main', String git_credentials = null, String git_url = null) {    
         echo "This is gitCheckout.groovy"
         checkout([$class: 'GitSCM', 
-                  branches: [[name: "*/${GIT_BRANCH}"]], 
-                  userRemoteConfigs: [[credentialsId: "${GIT_CREDS}", url: "${GIT_URL}"]]
+                  branches: [[name: "*/${git_branch}"]], 
+                  userRemoteConfigs: [[credentialsId: "${git_credentials}", url: "${git_url}"]]
                 ])             
 }
