54e1f1b6d2b9fbf62e12b13efd97d3d10f928b8a
Harsha Jain <jaintpharsha@gmail.com>
2023-09-28 09:49:29 +0530

    Docker build 28-September-23 09:49AM

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,10 +1,11 @@
 def call(String dockerfile = 'Dockerfile', 
+         String buildContext = null,    
          String image_name = null, 
          String image_tag = null,
          String registryURL = null,
          String registryCredentials = null) {    
         echo "This is dockerBuild.groovy"
-        def dockerImage = docker.build("${image_name}:${image_tag}", "-f ${} .")
+        def dockerImage = docker.build("${image_name}:${image_tag}", "-f ${dockerfile} ${buildContext}")
         docker.withRegistry("${registryURL}", "${registryCredentials}") {
             dockerImage.push()
         }    
