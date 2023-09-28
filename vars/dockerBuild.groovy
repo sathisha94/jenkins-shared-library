3089348f9dccd8e0279106bb1aae3ee78f3327ea
Harsha Jain <jaintpharsha@gmail.com>
2023-09-28 08:50:03 +0530

    dockerBuild 28-September-23 08:50AM

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,23 @@
+def call(String git_branch = 'main', String git_credentials = null, String git_url = null) {    
+    pipeline {
+        agent any 
+
+        environment {
+            GIT_BRANCH = "${git_branch}"
+            GIT_CREDS = "${git_credentials}"
+            GIT_URL = "${git_url}"
+        }
+
+        stages {
+            stage('Checkout code') {
+                steps {
+                    checkout([$class: 'GitSCM', 
+                              branches: [[name: "*/${GIT_BRANCH}"]], 
+                              userRemoteConfigs: [[credentialsId: "${GIT_CREDS}", url: "${GIT_URL}"]]
+                            ])
+                }
+            }
+        }
+    }
+
+}
