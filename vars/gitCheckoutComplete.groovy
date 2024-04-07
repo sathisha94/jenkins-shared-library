23c8b563f3acf9e98af08ff6016956595c96ce0c
Sathisha C <91380007+sathisha94@users.noreply.github.com>
2024-04-07 13:46:03 +0530

    Update gitCheckoutComplete.groovy

--- vars/gitCheckoutComplete.groovy
+++ vars/gitCheckoutComplete.groovy
@@ -10,9 +10,9 @@ def call(String git_branch = 'main', String git_credentials = null, String git_u
 
         stages {
             stage('Checkout code') {
-                s   echo "This is dockerBuildComplete.groovy"
-                    checkout([$class: 'GitSCM', 
-                 teps {
+                steps { 
+                    echo "This is dockerBuildComplete.groovy"
+                    checkout([$class: 'GitSCM',
                               branches: [[name: "*/${GIT_BRANCH}"]], 
                               userRemoteConfigs: [[credentialsId: "${GIT_CREDS}", url: "${GIT_URL}"]]
                             ])
